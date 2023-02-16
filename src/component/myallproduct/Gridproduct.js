bc2e497dbb50f0613cbd965db8f38c1439717ab9
pranishshrestha33 <pranishs97@gmail.com>
2023-02-16 19:30:36 +0545

    feat: list and grid products; grid product is still baki.

--- src/component/myallproduct/Gridproduct.js
+++ src/component/myallproduct/Gridproduct.js
@@ -1,38 +1,35 @@
-import React from 'react'
-import Featureproduct from '../featureproduct/Featureproduct' 
-import Card from 'react-bootstrap/Card';
-import { Link } from 'react-router-dom';
-import './Grid.css'
-const Gridproduct = ({productss}) => {
-     console.log("grid prod", productss)
+import React from "react";
+import Featureproduct from "../featureproduct/Featureproduct";
+import Card from "react-bootstrap/Card";
+import { Link } from "react-router-dom";
+import "./Grid.css";
+const Gridproduct = ({ productss }) => {
+  console.log("grid prod", productss);
 
   return (
-   <div className="gridproducts">
-   <div className="p">
-      {productss.map((curElem)=>{
-       const{id,title,thumbnail,price } = curElem;
-        return(
-          <div className="my">
-          <div className="feproducts" key={curElem.id}>
-          <Link to={`/singleproduct/${id}`}>
-          <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src={thumbnail} />
-          <Card.Body>
-            <Card.Title>{title}</Card.Title>
-            <Card.Text>
-              ${price}
-            </Card.Text>
-            
-          </Card.Body>
-        </Card>
-        </Link>
-       
-          </div>
-          </div> )
-      })}
+    <div className="gridproducts">
+      <div className="grid-products-container">
+        {productss.map((curElem) => {
+          const { id, title, thumbnail, price } = curElem;
+          return (
+            <div className="my">
+              <div className="feproducts" key={curElem.id}>
+                <Link to={`/singleproduct/${id}`}>
+                  <Card className="grid-card">
+                    <Card.Img variant="top" src={thumbnail} className="grid-image" />
+                    <Card.Body>
+                      <Card.Title>{title}</Card.Title>
+                      <Card.Text>Rs. ${price}</Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Link>
+              </div>
+            </div>
+          );
+        })}
       </div>
-   </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Gridproduct
+export default Gridproduct;
--- src/component/myallproduct/Listproduct.js
+++ src/component/myallproduct/Listproduct.js
@@ -1,30 +1,33 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import './Grid.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Grid.css";
+import "./listProduct.css"
 
-const Listproduct = ({productss}) => {
+const Listproduct = ({ productss }) => {
   return (
     <div className="listproduct">
-    <div className="listitems">
-    {productss.map((curElem)=>{
-      const{id,title,thumbnail,price,description } = curElem;
-      return <div className="returnlist" key={curElem.id}>
-           <div className="listimage">
-           <img src={thumbnail} alt="" />
-           </div>
-           <div className="listcontent">
-           <h2>{title}</h2>
-           <p>{price}</p>
-           <p>{description.slice(0 ,20)}...</p>
-           <Link to={`/singleproduct/${id}`}>
-           <button>See More</button>
-           </Link>
-           </div>
+      <div className="listitems">
+        {productss.map((curElem) => {
+          const { id, title, thumbnail, price, description } = curElem;
+          return (
+            <div className="returnlist" key={curElem.id}>
+              <div className="listimage">
+                <img src={thumbnail} alt="" className="listproduct-img"/>
+              </div>
+              <div className="listcontent">
+                <h2>{title}</h2>
+                <p>Rs. {price}</p>
+                <p>{description.slice(0, 60)}...</p>
+                <Link to={`/singleproduct/${id}`}>
+                  <button className="list-btn">See More</button>
+                </Link>
+              </div>
+            </div>
+          );
+        })}
       </div>
-    })}
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Listproduct
+export default Listproduct;
